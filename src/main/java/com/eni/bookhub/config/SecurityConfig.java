44a2797e2316436ff6b75db53c51b16893f30e63
Louis Pichon <louis.pichon17@gmail.com>
2026-04-23 11:02:04 +0200

    feat: Ajouts du backend pour récupérer le détail d'un livre + correction du fichiers scripts.sql + mise à jour de fonction dépréciés

--- src/main/java/com/eni/bookhub/config/SecurityConfig.java
+++ src/main/java/com/eni/bookhub/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.eni.bookhub.security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -13,7 +14,6 @@ public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
 
-    //  CONSTRUCTEUR
     public SecurityConfig(JwtFilter jwtFilter) {
         this.jwtFilter = jwtFilter;
     }
@@ -22,7 +22,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
--- src/main/java/com/eni/bookhub/dto/response/BookResponse.java
+++ src/main/java/com/eni/bookhub/dto/response/BookResponse.java
@@ -1,4 +1,36 @@
 package com.eni.bookhub.dto.response;
 
+import com.eni.bookhub.entity.Book;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
 public class BookResponse {
+
+    private final Integer id;
+    private final String titre;
+    private final String auteur;
+    private final String isbn;
+    private final LocalDate dateParution;
+    private final Integer nombrePages;
+    private final String description;
+    private final String urlCouverture;
+    private final Integer totalExemplaires;
+    private final Integer exemplairesDisponibles;
+    private final String categorie;
+
+    public BookResponse(Book book) {
+        this.id = book.getId();
+        this.titre = book.getTitre();
+        this.auteur = book.getAuteur();
+        this.isbn = book.getIsbn();
+        this.dateParution = book.getDateParution();
+        this.nombrePages = book.getNombrePages();
+        this.description = book.getDescription();
+        this.urlCouverture = book.getUrlCouverture();
+        this.totalExemplaires = book.getTotalExemplaires();
+        this.exemplairesDisponibles = book.getExemplairesDisponibles();
+        this.categorie = book.getCategorie().getNom();
+    }
 }
