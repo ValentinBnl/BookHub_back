9b6dae3cf6e79c5912d633b4501a23425f0beab8
Louis Pichon <louis.pichon17@gmail.com>
2026-04-24 16:25:23 +0200

    feat: Implémentation des réservations + ajout de globalexceptionhandler pour capter les erreurs métier et retourner une erreur json plutôt que spring

--- src/main/java/com/eni/bookhub/dto/request/ReservationRequest.java
+++ src/main/java/com/eni/bookhub/dto/request/ReservationRequest.java
@@ -1,4 +1,13 @@
 package com.eni.bookhub.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ReservationRequest {
+
+    @NotNull
+    private Integer bookId;
 }
--- src/main/java/com/eni/bookhub/repository/ReservationRepository.java
+++ src/main/java/com/eni/bookhub/repository/ReservationRepository.java
@@ -1,4 +1,25 @@
 package com.eni.bookhub.repository;
 
-public class ReservationRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eni.bookhub.entity.Reservation;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+
+    List<Reservation> findByUserId(Integer userId);
+
+    List<Reservation> findByBookIdAndStatusOrderByReservationDateAsc(Integer bookId, Reservation.Status status);
+
+    boolean existsByUserIdAndBookIdAndStatusIn(Integer userId, Integer bookId, List<Reservation.Status> statuses);
+
+    int countByBookIdAndStatus(Integer bookId, Reservation.Status status);
+
+    int countByUserIdAndStatusIn(Integer userId, List<Reservation.Status> statuses);
+
+    List<Reservation> findByBookIdAndStatusAndRankWaitingListGreaterThan(
+            Integer bookId, Reservation.Status status, Integer rank);
 }
